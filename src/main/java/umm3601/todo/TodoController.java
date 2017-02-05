43a85bcf51b293d222ff840ec5cedda75a0c33a8
cookx876 <cookx876@morris.umn.edu>
2017-02-05 17:29:04 -0600

    attempt orderBy and arbitrary filtering
    both attempts failed halfway through

--- src/main/java/umm3601/todo/TodoController.java
+++ src/main/java/umm3601/todo/TodoController.java
@@ -22,12 +22,12 @@ public class TodoController {
 
     // List Todos
     public Todo[] listTodos(Map<String, String[]> queryParams) {
-        Todo[] filteredTodos = todos;
+      //  Todo[] filteredTodos = todos;
 
         // page limit
         if(queryParams.containsKey("limit")){
             int limit = Integer.parseInt(queryParams.get("limit")[0]);
-            filteredTodos = getPageLimit(limit);
+            todos = getPageLimit(limit);
         }
 
         if(queryParams.containsKey("status")){
@@ -38,24 +38,38 @@ public class TodoController {
                 state =false;
             }
 
-            filteredTodos = returnstatuses(state);
+            todos = returnstatuses(state);
         }
 
         if(queryParams.containsKey("contains")){
             String search = queryParams.get("contains")[0];
-            filteredTodos = searchBody(search);
+            todos = searchBody(search);
         }
 
         if(queryParams.containsKey("owner")){
             String owner = queryParams.get("owner")[0];
-            filteredTodos = filterOwner(owner);
+            todos = filterOwner(owner);
         }
         if(queryParams.containsKey("category")){
             String category = queryParams.get("category")[0];
-            filteredTodos = filterCategory(category);
+            todos = filterCategory(category);
+        }
+        if(queryParams.containsKey("orderBy")){
+            String param = queryParams.get("orderBy")[0];
+            if(param.equals("owner")){
+                todos = orderByOwner(param);
+            } else if (param.equals("category")){
+                todos = orderByCategory(param);
+            } else if (param.equals("status")){
+                todos = orderByStatus(param);
+            } else if (param.equals("body")){
+                todos = orderByBody(param);
+            } else {
+                return null;
+            }
         }
 
-        return filteredTodos;
+        return todos;
     }
 
 
@@ -144,4 +158,40 @@ public class TodoController {
         return toReturn;
     }
 
+    //From rosettacode.org :)
+    public Todo[] orderByOwner (String owner){
+        Todo[] tmp = new Todo[todos.length];
+        int i =1; int j=0;
+        tmp[0] = todos[0];
+        while(i<todos.length){
+            String thing = todos[i].owner;
+            while(thing.compareTo(tmp[j].owner) < 0){
+                j++;
+            }
+            tmp[j].owner = thing;
+        }
+//        for (i =1; i<tmp.length;i++) {
+//            for (j = 0; j<i; j++) {
+//                if (tmp[i].owner.compareTo(tmp[j].owner) < 0) {
+//                    Todo foo = tmp[i];
+//                    tmp[i] = tmp[j];
+//                    tmp[j] = foo;
+//                }
+//            }
+//            i++;
+//        }
+        return tmp;
+    }
+
+    public Todo[] orderByCategory(String owner){
+        return null;
+    }
+
+    public Todo[] orderByBody(String owner){
+        return null;
+    }
+
+    public Todo[] orderByStatus(String owner){
+        return null;
+    }
 }
